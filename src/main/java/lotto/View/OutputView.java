38eabf535336d9ad4790f49ae59cc957506038cf
rndnjsgud <00kwh@naver.com>
2025-11-03 04:53:58 +0900

    feat(): 기존 출력과 다른 부분 변경
    프로그래밍 요구사항에서 원하는 출력과 다른 부분이 있어 수정

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,31 +1,41 @@
 package lotto.View;
 
 import lotto.Domain.Lotto;
+import lotto.Domain.LottoRank;
 import lotto.Domain.LottoRanks;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class OutputView {
-    public static void purchaseResultOutput(int amountOfLottos, List<Lotto> lottos){
+    public static void purchaseResultOutput(int amountOfLottos, List<Lotto> lottos) {
         System.out.println(amountOfLottos + "개를 구매했습니다.");
         lottos.forEach(lotto -> {
             System.out.println(lotto.getNumbers());
         });
     }
 
-    public static void totalResultOutput(LottoRanks lottoRanks){
+    public static void totalResultOutput(LottoRanks lottoRanks) {
         System.out.println("당첨 통계\n" + "---");
 
-        lottoRanks.getLottoRanks()
-                .forEach((lottoRank, count) -> {
-                    if(count > 2) {
-                        System.out.println(lottoRank.getLottoNumberMatchingCount() + "개 일치 ("
-                                +String.format("%,d", lottoRank.getPrize()) + "원) - " +count + "개");
+        Arrays.stream(LottoRank.values())
+                .forEach(lottoRank -> {
+                    int count = lottoRanks
+                            .getLottoRanks()
+                            .getOrDefault(lottoRank, 0);
+                    if(lottoRank.getPrize() != 30000000) {
+                        System.out.printf("%d개 일치 (%,d원) - %d개,", lottoRank.getLottoNumberMatchingCount()
+                                , lottoRank.getPrize(), count);
                     }
+                    else if(lottoRank.getPrize() == 30000000){
+                        System.out.printf("%d개 일치, 보너스 볼 일치 (%,d원) - %d개,", lottoRank.getLottoNumberMatchingCount()
+                                , lottoRank.getPrize(), count);
+                    }
+                    System.out.println();
                 });
     }
 
-    public static void earningRateOutput(double earningRate){
-        System.out.println("총 수익률은 " + earningRate + "% 입니다.");
+    public static void earningRateOutput(double earningRate) {
+        System.out.println("총 수익률은 " + earningRate + "%입니다.");
     }
 }
