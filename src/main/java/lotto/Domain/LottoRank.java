7364e9a74ff3dfdcc642dea346230faf321a9ec7
rndnjsgud <00kwh@naver.com>
2025-11-03 04:25:03 +0900

    refactor(): 출력 술서를 위해 Enum의 순서를 변경

--- src/main/java/lotto/Domain/LottoRank.java
+++ src/main/java/lotto/Domain/LottoRank.java
@@ -1,12 +1,13 @@
 package lotto.Domain;
 
 public enum LottoRank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
+    FAIL(2, false, 0),
     FIFTH(3, false, 5_000),
-    FAIL(2, false, 0);
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000);
+
 
     private final int lottoNumberMatchingCount;
     private final boolean isBonusNumberInLotto;
