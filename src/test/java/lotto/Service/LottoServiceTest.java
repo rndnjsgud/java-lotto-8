ed0cb740fa551fe92fe89b974d4673e29ee84bdd
rndnjsgud <00kwh@naver.com>
2025-11-02 23:09:25 +0900

    feat(): 랜덤한 숫자를 이용하여 로또를 생성하는 메소드 테스트 코드 작성
    메소드 테스트 실패 확인, createLotto 메소드 구현 필요

--- src/test/java/lotto/Service/LottoServiceTest.java
+++ src/test/java/lotto/Service/LottoServiceTest.java
@@ -1,9 +1,15 @@
 package lotto.Service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Domain.Lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoServiceTest {
@@ -17,6 +23,16 @@ class LottoServiceTest {
         //when
         int amountOfLottos = lottoService.calculateAmountOfLottos(amountOfMoney);
         //then
-        Assertions.assertThat(amountOfLottos).isEqualTo(8);
+        assertThat(amountOfLottos).isEqualTo(8);
+    }
+
+    @DisplayName("랜덤한 숫자를 이용하여 로또를 생성하는 기능")
+    @Test
+    void 랜덤한_숫자를_이용하여_로또를_생성(){
+        //given
+        //when
+        Lotto lotto = createLotto();
+        //then
+        assertThat(lotto).isNotNull();
     }
 }
