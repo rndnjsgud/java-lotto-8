ca605adbd0dd135860ae3e9259c267781949f532
rndnjsgud <00kwh@naver.com>
2025-11-03 04:57:53 +0900

    feat(): 작성한 코드들에 대한 구현 완료
    프로그래밍 요구사항을 따라 잘못된 입력 받을 시 오류문구를 출력하고 해당 입력부터 다시 입력받기 위해
    입력 시능 별 while문과 try-catch문을 이용하여 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,52 @@
 package lotto;
 
+import lotto.Controller.LottoController;
+import lotto.Service.LottoService;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoService lottoService = new LottoService();
+        LottoController lottoController = new LottoController(lottoService);
+
+        int amountOfMoney = 0;
+        List<Integer> winningNumbers;
+        int bonusNumber;
+
+        while (true) {
+            try {
+                amountOfMoney = lottoController.inputPurchaseMoneyAmount();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        while (true) {
+            try {
+                lottoController.createLottos(amountOfMoney);
+                winningNumbers = lottoController.inputWinningNumbers();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while (true) {
+            try {
+                bonusNumber = lottoController.inputBonusNumber(winningNumbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        try {
+            lottoController.printLottoResult(winningNumbers, bonusNumber);
+            lottoController.printEarningRate(winningNumbers, bonusNumber, amountOfMoney);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
